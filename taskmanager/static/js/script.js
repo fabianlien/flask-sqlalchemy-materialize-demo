4afe5332c3b4164e170aec9018bd238eb4e57c85
Fabian Lien <fabian.lien@gmail.com>
2022-04-22 13:11:21 +0000

    add template to add task and set up route to handle method on submit

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,14 @@
+// init sidenav
 $(document).ready(function(){
     $('.sidenav').sidenav();
+  });
+
+  // init datepicker
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+
+  // init select
+  $(document).ready(function(){
+    $('select').formSelect();
   });
